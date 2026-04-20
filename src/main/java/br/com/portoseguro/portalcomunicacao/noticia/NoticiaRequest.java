9e4828455d0c32bb2fe8cd7df1b7be9927e2223a
Caique Lima <caique.lima@vilt-group.com>
2026-04-20 11:57:45 -0300

    fix: padroniza formato de data para ISO 8601 no NoticiaRequest

--- src/main/java/br/com/portoseguro/portalcomunicacao/noticia/NoticiaRequest.java
+++ src/main/java/br/com/portoseguro/portalcomunicacao/noticia/NoticiaRequest.java
@@ -1,6 +1,5 @@
 package br.com.portoseguro.portalcomunicacao.noticia;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -9,7 +8,7 @@ public record NoticiaRequest(
         @NotBlank String titulo,
         @NotBlank String subtitulo,
         @NotBlank String conteudo,
-        @NotNull @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss") LocalDateTime dataPublicacao,
+        @NotNull LocalDateTime dataPublicacao,
         @NotNull Boolean ativo,
         @NotNull Long categoriaId,
         @NotNull Long autorId) {
